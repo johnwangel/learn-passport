f0e1d5e9e4c3bca7121bb73716554b60ecfbc504
Jesse Copeland <jesse.copeland@gmail.com>
2017-07-27 08:44:53 -1000

    Change users form Class to obj to simulate requireing a model

--- models/users.js
+++ models/users.js
@@ -1,39 +1,47 @@
-class Users {
-  constructor() {
-    this.data = [
-      {
-        id: 1,
-        username: 'passport-user',
-        password: 'password'
-      },
-      {
-        id: 2,
-        username: 'second-user',
-        password: 'passwd'
-      },
-      {
-        id: 3,
-        username: 'next-user',
-        password: 'nextword'
-      }
-    ];
+const users = [
+  {
+    id: 1,
+    username: 'passport-user',
+    password: 'password',
+    role: 'user'
+  },
+  {
+    id: 2,
+    username: 'second-user',
+    password: 'passwd',
+    role: 'user'
+  },
+  {
+    id: 3,
+    username: 'next-user',
+    password: 'nextword',
+    role: 'user'
+  },
+  {
+    id: 4,
+    username: 'admin-user',
+    password: 'adminpass',
+    role: 'admin'
   }
+];
 
-  findById(id, cb) {
-    let user = this.data.find(user => id === user.id);
-    if (user) {
-      return cb(null, user);
-    }
-    return cb(null);
+function findById(id, cb) {
+  let user = users.find(user => id === user.id);
+  if (user) {
+    return cb(null, user);
   }
-
-  findUser(userObj, cb) {
-    let user = this.data.find(user => userObj.username === user.username);
-    if (user) {
-      return cb(null, user);
-    }
-    return cb(null);
-  }
+  return cb(null);
 }
 
-module.exports = Users;
+function findUser(userObj, cb) {
+  let user = users.find(user => userObj.username === user.username);
+  if (user) {
+    return cb(null, user);
+  }
+  return cb(null);
+}
+
+module.exports = {
+  findById: findById,
+  findUser: findUser
+};
