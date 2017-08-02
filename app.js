27836d90888877084a5123ca01083416ca7870ac
John Atkins <johnatkins1999@yahoo.com>
2017-08-01 14:08:31 -1000

    requires

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
+/*jshint esversion: 6 */
 const Users = require('./models/users');
 // require packages
+const express = require('express');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 
 // invoke express
 
+
 // add middleware
+
+
+
 
 // add routes
 // app.get('/', (req, res) => {
